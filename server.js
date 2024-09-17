2e57b0c55e118416b0267c26964680ff10f69644
selenavuk <64490758+selenavuk@users.noreply.github.com>
2024-09-18 00:26:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
-const bodyParser = require('body-parser'); // For handling JSON requests
+// const bodyParser = require('body-parser'); // For handling JSON requests
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 // Middleware to parse JSON request bodies
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+
+
+const corsOptions = {
+  origin: 'https://rhv.rs', // Adjust this to your frontend domain
+  methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  allowedHeaders: 'Content-Type,Authorization',
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 
 // Configure CORS
 //app.use(cors());
 // Configure CORS to allow all origins
-app.use(cors({
-  origin: '*', // Allow all origins for development; restrict in production
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  allowedHeaders: 'Content-Type,Authorization',
-  credentials: true
-}));
+// app.use(cors({
+//   origin: 'https://rhv.rs', // Allow all origins for development; restrict in production
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+//   allowedHeaders: 'Content-Type,Authorization',
+//   credentials: true
+// }));
 
 // Proxy endpoint
 app.get('/proxy/:fileId', async (req, res) => {
@@ -33,28 +43,28 @@ app.get('/proxy/:fileId', async (req, res) => {
   }
 });
 
-// Proxy for all other API requests
-app.use('/api', async (req, res) => {
-  const targetUrl = `https://oks-api-production.up.railway.app${req.originalUrl}`;
+// // Proxy for all other API requests
+// app.use('/api', async (req, res) => {
+//   const targetUrl = `https://oks-api-production.up.railway.app${req.originalUrl}`;
 
-  try {
-    const response = await axios({
-      method: req.method,
-      url: targetUrl,
-      data: req.body,
-      headers: {
-        'Content-Type': req.headers['content-type'],
-        // Add other headers if needed
-      },
-      responseType: 'json',
-    });
+//   try {
+//     const response = await axios({
+//       method: req.method,
+//       url: targetUrl,
+//       data: req.body,
+//       headers: {
+//         'Content-Type': req.headers['content-type'],
+//         // Add other headers if needed
+//       },
+//       responseType: 'json',
+//     });
 
-    res.status(response.status).json(response.data);
-  } catch (error) {
-    console.error('Error proxying request:', error);
-    res.status(error.response ? error.response.status : 500).send('Error proxying request');
-  }
-});
+//     res.status(response.status).json(response.data);
+//   } catch (error) {
+//     console.error('Error proxying request:', error);
+//     res.status(error.response ? error.response.status : 500).send('Error proxying request');
+//   }
+// });
 
 app.listen(port, () => {
   console.log(`Proxy server is running`);
