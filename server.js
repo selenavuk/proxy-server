cddb226c62842f9b77b02e88595632a69a42bfbb
selenavuk <64490758+selenavuk@users.noreply.github.com>
2024-09-18 00:16:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
+const bodyParser = require('body-parser'); // For handling JSON requests
+
 const app = express();
 const port = process.env.PORT || 8080;
 
+// Middleware to parse JSON request bodies
+app.use(bodyParser.json());
+
 // Configure CORS
-app.use(cors());
+//app.use(cors());
+// Configure CORS to allow all origins
+app.use(cors({
+  origin: '*', // Allow all origins for development; restrict in production
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: 'Content-Type,Authorization',
+  credentials: true
+}));
 
 // Proxy endpoint
 app.get('/proxy/:fileId', async (req, res) => {
@@ -21,6 +33,29 @@ app.get('/proxy/:fileId', async (req, res) => {
   }
 });
 
+// Proxy for all other API requests
+app.use('/api', async (req, res) => {
+  const targetUrl = `https://oks-api-production.up.railway.app${req.originalUrl}`;
+
+  try {
+    const response = await axios({
+      method: req.method,
+      url: targetUrl,
+      data: req.body,
+      headers: {
+        'Content-Type': req.headers['content-type'],
+        // Add other headers if needed
+      },
+      responseType: 'json',
+    });
+
+    res.status(response.status).json(response.data);
+  } catch (error) {
+    console.error('Error proxying request:', error);
+    res.status(error.response ? error.response.status : 500).send('Error proxying request');
+  }
+});
+
 app.listen(port, () => {
-  console.log(`Proxy server is running on http://localhost:${port}`);
+  console.log(`Proxy server is running`);
 });
